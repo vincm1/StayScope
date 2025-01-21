d0432137f0889089b8180f4a479979bddb817388
Vincent Michler <60316541+vincm1@users.noreply.github.com>
2025-01-21 14:05:06 +0100

    test for vercel

--- frontend/components/Whitepaper.vue
+++ frontend/components/Whitepaper.vue
@@ -66,7 +66,7 @@ const handleSubmit = async () => {
   downloadButtonState.value = 'loading';
 
   try {
-    const response = await $fetch(`${apiBaseUrl}/api/contact`, {
+    const response = await $fetch(`/api/contact`, {
       method: 'POST',
       body: formData.value,
     });
